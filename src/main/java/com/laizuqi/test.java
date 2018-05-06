6a2848eeb976e62576afaed40094ef29677df295
laizuqi <329354111@qq.com>
2018-05-06 17:27:45 +0800

    commit

---
+++ src/main/java/com/laizuqi/test.java
@@ -0,0 +1,11 @@
+package com.laizuqi;
+
+/**
+ * Created by Archie on 2018/5/6.
+ */
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("hello-world");
+    }
+}
